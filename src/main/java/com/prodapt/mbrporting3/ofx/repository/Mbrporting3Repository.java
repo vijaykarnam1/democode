2f7dbea8b2e6ea172f7a45cd68952b88a81da663
vijaykarnam1 <167899568+vijaykarnam1@users.noreply.github.com>
2024-11-06 20:19:59 -0600

    Delete src/main directory

--- src/main/java/com/prodapt/mbrporting3/ofx/repository/Mbrporting3Repository.java
+++
@@ -1,9 +0,0 @@
-
-package com.prodapt.mbrporting3.ofx.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.prodapt.mbrporting3.ofx.entity.Mbrporting3Entity;
-
-public interface Mbrporting3Repository extends JpaRepository<Mbrporting3Entity, Long> {
-
-}
